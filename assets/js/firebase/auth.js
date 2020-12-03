8194a05d1e7d84b305f70af8fbe1a4b628a59417
Misael <manmisa2003@gmail.com>
2020-12-03 17:24:46 +0000

    auth

--- assets/js/firebase/auth.js
+++ assets/js/firebase/auth.js
@@ -39,3 +39,15 @@ function login(){
     })
 }
 
+// TODO NÃO FUNCIONA
+function logout(){
+    const logout = document.querySelector("#logout")
+    logout.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        auth.signOut().then(() => {
+            console.log('Utilizador fez logout')
+        })
+    })
+}
+
